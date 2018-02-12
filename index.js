e9a45a1a92e4400bc45da8fb2c175adde238fc50
Jean Marcellin <marcellinja@gmail.com>
2018-02-12 14:04:29 -0800

    - Changed app name - Changed comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//dependencies required for the app
+//Required dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
@@ -7,18 +7,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-//render css files
+//CSS file
 app.use(express.static("public"));
 
 var list_of_task = [];
 var completed_task = [];
 
+// Remove task
 app.post("/removetask", function(req, res) {
     var completed = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
+    //Checking type of file
     if (typeof completed === "string") {
         completed_task.push(completed);
-        //check if the completed task already exits in the task when checked, then remove it
+        //if exists then remove
         list_of_task.splice(list_of_task.indexOf(completed), 1);
     } else if (typeof completed === "object") {
         for (var i = 0; i < completed.length; i++) {
@@ -30,22 +31,22 @@ app.post("/removetask", function(req, res) {
 });
 
 
-//post route for adding new task
+//Add task
 app.post("/addtask", function(req, res) {
     var newTask = req.body.newtask;
-    //add the new task from the post route
+    //push new task in list
     list_of_task.push(newTask);
     res.redirect("/");
 });
 
 
-//render the ejs and display added task, completed task
+//render the app
 app.get("/", function(req, res) {
     if(list_of_task == undefined) res.render("index");
     else res.render("index", { list_of_task: list_of_task, completed_task: completed_task });
 });
 
-//set app to listen on port 3000
+//listen on port 3000
 app.listen(3000, function() {
     console.log("server is running on port 3000");
 });
